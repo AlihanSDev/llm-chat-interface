6a1f154db9495e69852fadc2723e4df02e5f7490
AlihanSDev <ali70s@mail.ru>
2025-11-26 22:21:22 +0600

    Improving preproduction configurations

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false,
+        timeout: 5000,
+        proxyTimeout: 5000,
+        rewrite: path => path.replace(/^\/api/, '')
       },
       '/socket.io': {
         target: 'http://localhost:3000',
         ws: true,
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false
       }
     }
   }
